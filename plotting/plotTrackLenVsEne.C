46e2c17cc2502a64b1b0a32a7ec21a9805597799
Gajendra Gurung <gajendra.315.gurung@gmail.com>
2023-03-08 20:12:25 -0600

    updated plotting function

--- plotting/plotTrackLenVsEne.C
+++ plotting/plotTrackLenVsEne.C
@@ -1,86 +1,90 @@
 void plotTrackLenVsEne(){
+	TFile* file = new TFile("../bremOutput.root", "read");
+	TTree* ana = (TTree*)file->Get("brem");
 
+	Double_t nEntries = ana->GetEntries();
 
-  TFile* file = new TFile("bremOutput.root", "read");
-  TTree* ana = (TTree*)file->Get("brem");
+	cout << "The root file has : "<< nEntries << " Entries" << endl;
+	Int_t           TrackID;
+	Double_t        fKineticEnergy;
+	Double_t        fTrackLength_X0;
+	Char_t          fProcess[16];
+	// List of branches
+	ana->SetBranchAddress("TrackID" , &TrackID);
+	ana->SetBranchAddress("fKineticEnergy", &fKineticEnergy);
+	ana->SetBranchAddress("fTrackLength_X0", &fTrackLength_X0);
+	ana->SetBranchAddress("fProcess", &fProcess);
 
-  Int_t           TrackID;
-  Double_t        fKineticEnergy;
-  Double_t        fTrackLength_X0;
-  Char_t          fProcess[16];
-  // List of branches
-  ana->SetBranchAddress("TrackID" , &TrackID);
-  ana->SetBranchAddress("fKineticEnergy", &fKineticEnergy);
-  ana->SetBranchAddress("fTrackLength_X0", &fTrackLength_X0);
-  ana->SetBranchAddress("fProcess", &fProcess);
+	// Initialize the 1-D histogram for each slice of energy
+	TH1D* slice1= new TH1D("slice1", "200 MeV", 100, 0, 18);
+	TH1D* slice3 = new TH1D("slice3", "1 GeV", 100, 0, 18);
+	TH1D* slice2 = new TH1D("slice2", "10 GeV", 100, 0, 18);
 
-  TH1D* slice1= new TH1D("slice1", "200 MeV", 100, 0, 18);
-  //	slice1->SetFillColor(3);
-  //	slice1->SetFillStyle(3004);
-  slice1->SetLineColor(3);
+	// Initialize the 2-D histogram for plotting Track length Vs. Energy distribution
+	TH2D* TrkLenVsEne = new TH2D("TrkLenVsEne", "Track Length Vs. Energy; Energy MeV; Tracklength/X_{0}", 5000, 0, 100000, 1000, 0, 18);
+	TrkLenVsEne->Sumw2();
+	
+	// Filling the histograms
+	for(Int_t i = 0; i < ana->GetEntries(); i ++){
+		ana->GetEntry(i);
+		if( TrackID != 1) continue;
+		
+		TrkLenVsEne->Fill(fKineticEnergy, fTrackLength_X0);
+	/* // This is one way to fill the projection histograms, works well if you want to specify a different bin width than original 2D plot
+		if(fKineticEnergy > 190 && fKineticEnergy < 210){slice1->Fill(fTrackLength_X0);}
+		if(fKineticEnergy > 990 && fKineticEnergy < 1010){slice2 ->Fill(fTrackLength_X0);}
+		if(fKineticEnergy > 9990 && fKineticEnergy < 10010){slice3 ->Fill(fTrackLength_X0);} */
+	}
 
-  TH1D* slice3 = new TH1D("slice3", "1 GeV", 100, 0, 18);
-  //	slice3->SetFillColor(4);
-  //	slice3->SetFillStyle(3006);
-  slice3->SetLineColor(4);
+	// Here we normalize the 2-D histogram
+	TH2D* TrkLenVsEne_norm = (TH2D*)TrkLenVsEne->Clone("norm");
+	TrkLenVsEne_norm->SetTitle("Normalized: Track Length Vs. Energy");
+	TrkLenVsEne_norm->Scale(1./TrkLenVsEne_norm->Integral(), "width");
 
-  TH1D* slice2 = new TH1D("slice2", "10 GeV", 100, 0, 18);
-  //	slice2->SetFillColor(7);
-  //	slice2->SetFillStyle(3007);
-  slice2->SetLineColor(7);
+	// Taking the slice at Energy of Interest (eoi)
+	double eoi = 200;
+	int pbin = TrkLenVsEne_norm->GetXaxis()->FindBin(eoi); //get the bin the energy is in
+	slice1 = TrkLenVsEne_norm->ProjectionY("slice1", pbin, pbin+1, "e1p"); //fill in the projection data
+	slice1->SetLineColor(3);
+	slice1->SetTitle("Tracklength Distribution for the given energies #pm 10 MeV; Tracklength/X_{0}; Counts / NbEntries");
+	
+	eoi = 1000;
+	pbin = TrkLenVsEne_norm->GetXaxis()->FindBin(eoi);
+	slice2 = TrkLenVsEne_norm->ProjectionY("slice2", pbin, pbin+1, "e1p");
+	slice2->SetLineColor(4);
+	
+	eoi = 10000;
+	pbin = TrkLenVsEne_norm->GetXaxis()->FindBin(eoi);
+	slice3 = TrkLenVsEne_norm->ProjectionY("slice3", pbin, pbin+1, "e1p");
+	slice3->SetLineColor(7);
 
+	auto c = new TCanvas("c","c");
+	TrkLenVsEne -> Draw("colz");
+	TrkLenVsEne ->SetContour(99);
+	c->SetLogx(1); //set the logarithmic scale on the X axis
+	c->SetLogy(1);
+	c->SetLogz(1);
 
+	auto c1 = new TCanvas("c1","c1");
+	TrkLenVsEne_norm -> Draw("colz");
+	TrkLenVsEne_norm ->SetContour(99);
+	c1->SetLogx(1); //set the logarithmic scale on the X axis
+	c1->SetLogy(1);
+	c1->SetLogz(1);
+	TrkLenVsEne_norm->SetMinimum(1e-10);
 
-  TH2D* TrkLenVsEne = new TH2D("TrkLenVsEne", "Track Length Vs. Energy; Energy MeV; Tracklength/X_{0}", 1000, 0, 100100, 1000, 0, 20);
-  for(Int_t i = 0; i < ana->GetEntries(); i ++){
-    ana->GetEntry(i);
-    if( TrackID != 1) continue;
-    TrkLenVsEne->Fill(fKineticEnergy, fTrackLength_X0);
-    if(fKineticEnergy > 190 && fKineticEnergy < 210){slice1->Fill(fTrackLength_X0);};
-    if(fKineticEnergy > 990 && fKineticEnergy < 1010){slice2 ->Fill(fTrackLength_X0);};
-    if(fKineticEnergy > 9990 && fKineticEnergy < 10010){slice3 ->Fill(fTrackLength_X0);}
-  }
-
-  /*
-  double eoi = 200;
-  int pbin = TrkLenVsEne->GetXaxis()->FindBin(eoi);
-  slice1 = TrkLenVsEne->ProjectionY("slice1", pbin, pbin+1, "e1p");
-  slice1->SetLineColor(5);
-  eoi = 1000;
-  pbin = TrkLenVsEne->GetXaxis()->FindBin(eoi);
-  slice2 = TrkLenVsEne->ProjectionY("slice2", pbin, pbin+1, "e1p");
-  slice2->SetLineColor(6);
-  eoi = 10000;
-  pbin = TrkLenVsEne->GetXaxis()->FindBin(eoi);
-  slice3 = TrkLenVsEne->ProjectionY("slice2", pbin, pbin+1, "e1p");
-  slice3->SetLineColor(7);
-  */
-
-
-
-
-
-  auto c = new TCanvas("c","c");
-  TrkLenVsEne -> Draw("colz");
-  TrkLenVsEne ->SetContour(99);
-  c->SetLogx(1); //set the logarithmic scale on the X axis
-  c->SetLogy(1);
-  c->SetLogz(1);
-
-
-  auto c1 = new TCanvas("c1","c1");
-
-  slice1->Draw();
-  slice2->Draw("same");
-  slice3->Draw("same");
-  c1->SetLogy(1);
-  gStyle->SetOptStat(0); //this removes the Statbox
-  auto legend = new TLegend();
-  legend->SetHeader("Legend","C");
-  legend->AddEntry(slice1,"200 MeV","l");
-  legend->AddEntry(slice2,"1 GeV","l");
-  legend->AddEntry(slice3,"10 GeV", "l");
-  legend->Draw();
-  //    file->Close();
+	auto c2 = new TCanvas("c2","c2");
+	slice1->Draw("");
+	slice2->Draw("same");
+	slice3->Draw("same");
+	c2->SetLogy(1);
+	gStyle->SetOptStat(0); //this removes the Statbox
+	auto legend = new TLegend();
+	legend->SetHeader("Legend","C");
+	legend->AddEntry(slice1,"200 MeV","l");
+	legend->AddEntry(slice2,"1 GeV","l");
+	legend->AddEntry(slice3,"10 GeV", "l");
+	legend->Draw();
+	//    file->Close();
 }
 
